4cc9cac0e35be2db2090e706d22204dca9366ab4
playerO1 <alekseyk555@mail.ru>
2021-06-14 22:30:47 +0300

    Finish wrote readme, add 1 unit test, fixex maven configuration

---
+++ src/test/java/simplegraph/onfile/FileGraphTest.java
@@ -0,0 +1,81 @@
+package simplegraph.onfile;
+
+import java.util.List;
+import java.io.File;
+import java.io.IOException;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import simplegraph.*;
+import simplegraph.exception.PathNotFoundException;
+
+/**
+ *
+ * @author A.K. = playerO1@github.com
+ */
+public class FileGraphTest {
+    static File tempPath;
+    
+    public FileGraphTest() {
+    }
+    
+    @BeforeClass // todo should be use BeforeEach/AfterEach and true clean temp directory?
+    public static void setUpClass() {
+        tempPath=new File("target/tmp_testpath");
+        if (!tempPath.isDirectory()) {
+          if (!tempPath.mkdir()) throw new RuntimeException("Can not make temp directory "+tempPath);
+        }
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+        if (tempPath.isDirectory()) {
+          // todo clear directory. Maven "mvn clean" should do clean target/* too.
+        }
+    }
+
+    @Test
+    public void makeGraphTest() throws IOException{
+        FileGraph<String> graph=new FileGraph<>(tempPath);
+        assertTrue("Not exist directory should be creaded by FileGraph", tempPath.isDirectory());
+        graph.addVertex("A");
+        graph.addVertex("B");
+        graph.addVertex("C");
+        graph.addEdge("A", "B", 1.0); // from, to, w
+        graph.addEdge("A", "B", 1.0);
+        graph.addEdge("A", "C", 15.4);
+        graph.addEdge("B", "C", 4.32);
+        graph.addEdge("C", "B", 6.0);
+        
+        assertEquals(3, graph.vertices());
+        assertEquals(5, graph.edges());
+        assertEquals(graph.vertices(), graph.getAllVertex().size());
+        assertNotNull(graph.vertexForObejct("A"));
+        //TODO toString check
+        //graph.close(); fixme assertion error, cause temp path has not clean after previous test.
+    }
+    
+    @Test
+    public void findPathTest() throws PathNotFoundException, IOException{
+        FileGraph<String> graph=new FileGraph<>(tempPath); // ISimpleGraph
+        graph.addVertex("A");
+        graph.addVertex("B");
+        graph.addVertex("C");
+        graph.addEdge("A", "B", 1.0); // from, to, w
+        graph.addEdge("A", "B", 1.0);
+        graph.addEdge("A", "C", 15.4);
+        graph.addEdge("B", "C", 4.32);
+        graph.addEdge("C", "B", 6.0);
+        
+        IPathFinder<String> pathF=graph.pathFinder();
+        List<String> path=pathF.computePath("A","C");
+        assertEquals(3, path.size());
+        assertEquals(1.0+4.32, pathF.getPathLength(), 0.0000001);
+        assertEquals("[A, B, C]",path.toString()); // String check it is not best solution, but fast and human readable.
+        graph.close();
+    }
+    
+}
